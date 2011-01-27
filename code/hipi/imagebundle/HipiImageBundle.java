29914308e52fc3649122808d86b20ec5c0aae8e4
svn <svn@0c4b1d1f-a87e-43e2-b515-cbd7e906cf19>
2011-01-27 05:28:25 +0000

    HipiImageBundle.java added support for metadata (index file) The index of offsets is important to implement InputSplit in Hadoop efficiently. Prior to this commit, HipiImageBundle uses one big file and very simple file format: (signature, next needle length, next needle type, needle binary blob until EOF) which doesn't have index of offsets embedded in the file. This commit fixed the problem by introducing two file structure: a index file and a dat file. The normal assigned Path in open will designate index file only. The index file structure is a bit more craftied in the sense that may support extended functionalities in the future:
    The first 4-byte is 0x81911b18, as signature for hipi bundle
    the following 2-byte records the length of data filename (with
    assumption that it will live in the same directory as index file)
    the following various bytes will have data filename encoded in UTF-8
    a following 8-byte will record image count (-1 for unknown)
    following 16-byte is "reserved" field
    following 4-byte is skip length, meaning how much should be skipped
    before the start of offset record (you may have more data in between)
    then, there starts offset record until EOF
    
    The data file is simplified as only having image length, image type and
    image blob. Conceptually, you can concatenate two data file togather if
    you file system support such operation atomically.
    
    The best part of current design is, you can even ignore concatenation
    operation when do merge (another important operation we eventually
    needs to support) by having index file to index several files at once
    (using reserve fields and skipped fields). Since index file is much
    smaller than actual data file, the operation can be fast.
    
    The downside, of course, is the fact that we have two files in the
    system rather than one which may affect the durability of our bundle.
    The good news is, index file can always recovered from data file, but
    takes some time.
    
    git-svn-id: svn+ssh://galicia/usr/local/svn/HIPI@13 0c4b1d1f-a87e-43e2-b515-cbd7e906cf19

--- code/hipi/imagebundle/HipiImageBundle.java
+++ code/hipi/imagebundle/HipiImageBundle.java
@@ -19,14 +19,16 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class HipiImageBundle extends AbstractImageBundle {
-	
+
 	private static final Log LOG = LogFactory.getLog(HipiImageBundle.class
 			.getName());
 
-	protected DataInputStream _input_stream = null;
-	protected DataOutputStream _output_stream = null;
-	protected BufferedInputStream _buffered_input_stream = null;
-	
+	protected DataInputStream _index_input_stream = null;
+	protected DataOutputStream _index_output_stream = null;
+	protected DataInputStream _data_input_stream = null;
+	protected DataOutputStream _data_output_stream = null;
+	protected BufferedInputStream _buffered_data_input_stream = null;
+
 	/**
 	 * A set of records where the key is the length of each image and the value
 	 * is the image type.
@@ -36,51 +38,109 @@ public class HipiImageBundle extends AbstractImageBundle {
 	private byte _sig[] = new byte[8];
 	private int _cacheLength = 0;
 	private int _cacheType = 0;
-	
+	private long _countingOffset = 0;
+	private Path _index_file = null;
+	private Path _data_file = null;
+	private long _imageCount = -1;
+
 	public HipiImageBundle(Configuration conf) {
 		super(conf);
 	}
 
+	private void writeBundleHeader() throws IOException {
+		// the index file header designed like this:
+		// 4-byte magic signature (0x81911618) for "HIPIIbIH"
+		// 2-byte (short int) to denote the length of data file name
+		// variable bytes of data file name
+		// 8-byte of image count (not mandated)
+		// 16-byte of reserved field
+		// 4-byte points to how much to skip in order to reach the start of
+		// offsets (default 0)
+		// 8-byte of offsets starts from here until EOF
+		_index_output_stream.writeInt(0x81911b18);
+		String data_name = _data_file.getName();
+		// write out filename in UTF-8 encoding
+		byte[] name_byte = data_name.getBytes("UTF-8");
+		_index_output_stream.writeShort(name_byte.length);
+		_index_output_stream.write(name_byte);
+		// write out image count (default -1 (unknown count))
+		_index_output_stream.writeLong(-1);
+		// write out reserved field
+		_index_output_stream.writeLong(0);
+		_index_output_stream.writeLong(0);
+		// skip 0 to reach start offset (potentially, you could put some
+		// metadata in between
+		_index_output_stream.writeInt(0);
+	}
+
 	@Override
 	protected void openForWrite(Path output_file) throws IOException {
-		// Should never happen because super class ensures this
-		if (_output_stream != null || _input_stream != null) {
+		// Check if the instance is already in some read/write states
+		if (_data_output_stream != null || _data_input_stream != null
+				|| _index_output_stream != null || _index_input_stream != null) {
 			throw new IOException("File " + output_file.getName()
 					+ " already opened for reading/writing");
 		}
-		
-		// This creates a new file. Since the super class checks for file
-		// existence before calling this, and this is a protected method, no
-		// user should ever be able to violate the assumption that once you are
-		// here you know the file either doesn't exit or should be overwritten.
-		_output_stream = new DataOutputStream(FileSystem.get(_conf).create(
-				output_file));
-		byte sig[] = {'H', 'I', 'P', 'I'};
-		_output_stream.write(sig);
+
+		// HipiImageBundle will create two files: an index file and a data file
+		// by default, the Path of output_file is the Path of index file, and
+		// data
+		// file will simply append .dat suffix
+		_index_file = output_file;
+		_index_output_stream = new DataOutputStream(FileSystem.get(_conf)
+				.create(_index_file));
+		_data_file = output_file.suffix(".dat");
+		_data_output_stream = new DataOutputStream(FileSystem.get(_conf)
+				.create(_data_file));
+		_countingOffset = 0;
+		writeBundleHeader();
 	}
 
 	private void readBundleHeader() throws IOException {
 		// check the signature in head
-		byte sig[] = new byte[4];
-		_buffered_input_stream.read(sig);
-		if (sig[0] != 'H' || sig[1] != 'I' || sig[2] != 'P' || sig[3] != 'I') {
+		int sig = _index_input_stream.readInt();
+		if (sig != 0x81911b18) {
 			throw new IOException("not a hipi image bundle");
+		}
+		short name_len = _index_input_stream.readShort();
+		byte[] name_byte = new byte[name_len];
+		_index_input_stream.read(name_byte);
+		String data_name = new String(name_byte, "UTF-8");
+		_data_file = new Path(_index_file.getParent(), data_name);
+		_imageCount = _index_input_stream.readLong();
+		// using readLong instead of skip because skip doesn't insure you can
+		// actually skip that much, and if readLong reached the EOF, will throw
+		// error anyway
+		_index_input_stream.readLong();
+		_index_input_stream.readLong();
+		int skipOver = _index_input_stream.readInt();
+		while (skipOver > 0) {
+			long skipped = _index_input_stream.skip(skipOver);
+			if (skipped <= 0)
+				break;
+			skipOver -= skipped;
 		}
 		_cacheLength = _cacheType = 0;
 	}
-	
+
 	@Override
 	protected void openForRead(Path input_file) throws IOException {
-		if (_input_stream != null || _output_stream != null) {
+		if (_data_output_stream != null || _data_input_stream != null
+				|| _index_output_stream != null || _index_input_stream != null) {
 			throw new IOException("File " + input_file.getName()
 					+ " already opened for reading/writing");
 		}
-		
-		_input_stream = new DataInputStream(FileSystem.get(_conf).open(
-				input_file));
-		_buffered_input_stream = new BufferedInputStream(_input_stream);
+
+		_index_file = input_file;
+		_index_input_stream = new DataInputStream(FileSystem.get(_conf).open(
+				_index_file));
 
 		readBundleHeader();
+
+		_data_input_stream = new DataInputStream(FileSystem.get(_conf).open(
+				_data_file));
+		_buffered_data_input_stream = new BufferedInputStream(
+				_data_input_stream);
 	}
 
 	@Override
@@ -90,31 +150,35 @@ public class HipiImageBundle extends AbstractImageBundle {
 		image_stream.read(data);
 		_cacheLength = data.length;
 		_cacheType = type.toValue();
-		_sig[0] = (byte)(_cacheLength >> 24);
-		_sig[1] = (byte)((_cacheLength >> 16) & 0xff);
-		_sig[2] = (byte)((_cacheLength >> 8) & 0xff);
-		_sig[3] = (byte)(_cacheLength & 0xff);
-		_sig[4] = (byte)(_cacheType >> 24);
-		_sig[5] = (byte)((_cacheType >> 16) & 0xff);
-		_sig[6] = (byte)((_cacheType >> 8) & 0xff);
-		_sig[7] = (byte)(_cacheType & 0xff);
-		_output_stream.write(_sig);
-		_output_stream.write(data);
+		_sig[0] = (byte) (_cacheLength >> 24);
+		_sig[1] = (byte) ((_cacheLength >> 16) & 0xff);
+		_sig[2] = (byte) ((_cacheLength >> 8) & 0xff);
+		_sig[3] = (byte) (_cacheLength & 0xff);
+		_sig[4] = (byte) (_cacheType >> 24);
+		_sig[5] = (byte) ((_cacheType >> 16) & 0xff);
+		_sig[6] = (byte) ((_cacheType >> 8) & 0xff);
+		_sig[7] = (byte) (_cacheType & 0xff);
+		_data_output_stream.write(_sig);
+		_data_output_stream.write(data);
+		_index_output_stream.writeLong(_countingOffset);
+		_countingOffset += 8 + data.length;
 	}
 
 	@Override
 	public long getImageCount() {
-		return -1;
+		return _imageCount;
 	}
 
 	@Override
 	protected ImageHeader readHeader() throws IOException {
 		if (_cacheLength > 0) {
-			ImageDecoder decoder = CodecManager.getDecoder(ImageType.fromValue(_cacheType));
+			ImageDecoder decoder = CodecManager.getDecoder(ImageType
+					.fromValue(_cacheType));
 			if (decoder == null)
 				return null;
-			ImageHeader header = decoder.decodeImageHeader(_buffered_input_stream);
-			_buffered_input_stream.reset();
+			ImageHeader header = decoder
+					.decodeImageHeader(_buffered_data_input_stream);
+			_buffered_data_input_stream.reset();
 			return header;
 		}
 		return null;
@@ -123,11 +187,12 @@ public class HipiImageBundle extends AbstractImageBundle {
 	@Override
 	protected FloatImage readImage() throws IOException {
 		if (_cacheLength > 0) {
-			ImageDecoder decoder = CodecManager.getDecoder(ImageType.fromValue(_cacheType));
+			ImageDecoder decoder = CodecManager.getDecoder(ImageType
+					.fromValue(_cacheType));
 			if (decoder == null)
 				return null;
-			FloatImage image = decoder.decodeImage(_buffered_input_stream);
-			_buffered_input_stream.reset();
+			FloatImage image = decoder.decodeImage(_buffered_data_input_stream);
+			_buffered_data_input_stream.reset();
 			return image;
 		}
 		return null;
@@ -137,43 +202,52 @@ public class HipiImageBundle extends AbstractImageBundle {
 	protected boolean prepareNext() {
 		try {
 			while (_cacheLength > 0) {
-				long skipped = _buffered_input_stream.skip((long) _cacheLength);
-				if (skipped == 0)
+				long skipped = _buffered_data_input_stream
+						.skip((long) _cacheLength);
+				if (skipped <= 0)
 					break;
 				_cacheLength -= skipped;
 			}
-			_buffered_input_stream.read(_sig);
-			if (_sig[0] != 0 || _sig[1] != 0 || _sig[2] != 0 || _sig[3] != 0 || _sig[4] != 0 || _sig[5] != 0 || _sig[6] != 0 || _sig[7] != 0) {
-				_cacheLength = ((_sig[0] & 0xff) << 24) | ((_sig[1] & 0xff) << 16) | ((_sig[2] & 0xff) << 8) | (_sig[3] & 0xff);
-				_cacheType = ((_sig[4] & 0xff) << 24) | ((_sig[5] & 0xff) << 16) | ((_sig[6] & 0xff) << 8) | (_sig[7] & 0xff);
-				_buffered_input_stream.mark(_cacheLength);
-				return true;
-			}
-			return false;
+			int byteRead = _buffered_data_input_stream.read(_sig);
+			if (byteRead <= 0)
+				return false;
+			_cacheLength = ((_sig[0] & 0xff) << 24) | ((_sig[1] & 0xff) << 16)
+					| ((_sig[2] & 0xff) << 8) | (_sig[3] & 0xff);
+			_cacheType = ((_sig[4] & 0xff) << 24) | ((_sig[5] & 0xff) << 16)
+					| ((_sig[6] & 0xff) << 8) | (_sig[7] & 0xff);
+			_buffered_data_input_stream.mark(_cacheLength);
+			return true;
 		} catch (IOException e) {
 			LOG.info("reach the end of the file");
 			return false;
 		}
 	}
-	
+
 	@Override
 	public void merge(AbstractImageBundle[] bundles) {
-		
+
 	}
 
 	@Override
 	public void close() throws IOException {
-		if (_buffered_input_stream != null) {
-			_buffered_input_stream.close();
+		if (_buffered_data_input_stream != null) {
+			_buffered_data_input_stream.close();
 		}
 
-		if (_input_stream != null) {
-			_input_stream.close();
+		if (_data_input_stream != null) {
+			_data_input_stream.close();
 		}
-		if (_output_stream != null) {
-			_sig[0] = _sig[1] = _sig[2] = _sig[3] = _sig[4] = _sig[5] = _sig[6] = _sig[7] = 0;
-			_output_stream.write(_sig);
-			_output_stream.close();
+
+		if (_index_input_stream != null) {
+			_index_input_stream.close();
+		}
+
+		if (_data_output_stream != null) {
+			_data_output_stream.close();
+		}
+
+		if (_index_output_stream != null) {
+			_index_output_stream.close();
 		}
 	}
 

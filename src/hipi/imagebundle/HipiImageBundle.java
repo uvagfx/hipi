77277d597dab84ba412f7b1ed0e7b18c547d96c5
Pete Warden <pete@jetpac.com>
2013-06-05 11:10:48 -0700

    Added debug logging to meta-data output

--- src/hipi/imagebundle/HipiImageBundle.java
+++ src/hipi/imagebundle/HipiImageBundle.java
@@ -422,6 +422,8 @@ public class HipiImageBundle extends AbstractImageBundle {
     byte metaData[] = null;
     if (header != null) {
       metaData = header.getMetaDataAsBytes();
+      String jsonText = new String(metaData, "UTF-8");
+      System.err.println("Writing out metaData " + jsonText);
     }
 		_cacheLength = (metaDataLength + imageData.length);
 		_cacheType = ((metaDataLength << 8) | (type.toValue() & 0xff));

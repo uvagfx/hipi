6d5ef226f16e0b304587fef0f392999c05ccf9dd
Zack Verham <zdv8rb@virginia.edu>
2015-08-13 16:09:38 -0400

    starting on restructuring of opencvmatutils

--- core/src/main/java/org/hipi/opencv/OpenCVUtils.java
+++ core/src/main/java/org/hipi/opencv/OpenCVUtils.java
@@ -1,16 +1,28 @@
 package org.hipi.opencv;
 
+
+import static org.hipi.image.PixelArray.TYPE_BYTE;
+import static org.hipi.image.PixelArray.TYPE_SHORT;
+import static org.hipi.image.PixelArray.TYPE_USHORT;
+import static org.hipi.image.PixelArray.TYPE_INT;
+import static org.hipi.image.PixelArray.TYPE_FLOAT;
+import static org.hipi.image.PixelArray.TYPE_DOUBLE;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_RGB2GRAY;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_GRAY2RGB;
 import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
 
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_highgui;
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Size;
 import org.bytedeco.javacpp.opencv_core.Scalar;
 import org.bytedeco.javacpp.indexer.FloatIndexer;
 import org.bytedeco.javacpp.indexer.Indexer;
@@ -20,101 +32,123 @@ import org.hipi.image.HipiImage.HipiImageType;
 import org.hipi.image.HipiImageHeader;
 import org.hipi.image.HipiImageHeader.HipiColorSpace;
 import org.hipi.image.HipiImageHeader.HipiImageFormat;
+import org.hipi.image.PixelArray;
+import org.hipi.image.RasterImage;
 import org.hipi.util.ByteUtils;
 
 public class OpenCVUtils {
   
-  public enum OpenCVOutputColorSpace {
-    OPENCV_RGB, OPENCV_GRAY
-  }
+  private static int[][] openCVTypeLookupTable = new int[][] {{opencv_core.CV_8UC1, opencv_core.CV_8UC2, opencv_core.CV_8UC3, opencv_core.CV_8UC4},
+                                                              {opencv_core.CV_16UC1, opencv_core.CV_16UC2, opencv_core.CV_16UC3, opencv_core.CV_16UC4},
+                                                              {opencv_core.CV_16SC1, opencv_core.CV_16SC2, opencv_core.CV_16SC3, opencv_core.CV_16SC4},
+                                                              {opencv_core.CV_32SC1, opencv_core.CV_32SC2, opencv_core.CV_32SC3, opencv_core.CV_32SC4},
+                                                              {opencv_core.CV_32FC1, opencv_core.CV_32FC2, opencv_core.CV_32FC3, opencv_core.CV_32FC4},
+                                                              {opencv_core.CV_64FC1, opencv_core.CV_64FC2, opencv_core.CV_64FC3, opencv_core.CV_64FC4}};
   
-  
-  public static FloatImage convertMatToFloatImage(Mat inputMat, HipiColorSpace outputHipiColorSpace, HipiImageFormat outputHipiImageFormat) throws IOException {
-   
-    //ensure that base mat data type is compatible with FloatImage
-    inputMat.convertTo(inputMat, opencv_core.CV_32F);
+  public static int generateOpenCVType(int pixelArrayDataType, int numBands) {
     
-    //Step 1: convert input mat (if necessary) to meet hipiOutputColorSpace specification
-    Mat convertedMat;
-    switch(outputHipiColorSpace) {
-      case RGB:
-        if(inputMat.channels() != 3) {
-          convertedMat = new Mat(inputMat.rows(), inputMat.cols(), opencv_core.CV_32FC3);
-          cvtColor(inputMat, convertedMat, CV_GRAY2RGB);
-        } else {
-          convertedMat = inputMat;
-        }
-        break;
-      case LUM:
-        if(inputMat.channels() != 1) {
-          convertedMat = new Mat(inputMat.rows(), inputMat.cols(), opencv_core.CV_32FC1);
-          cvtColor(inputMat, convertedMat, CV_RGB2GRAY);
-        } else {
-          convertedMat = inputMat;
-        }
-        break;
-     default:
-       throw new IOException("Unsupported hipi color space specified for output FloatImage [" + outputHipiColorSpace + "].");
+    int depthIndex = pixelArrayDataType;
+    int channelIndex = numBands - 1;
+    
+    if(depthIndex < 0 || depthIndex >= openCVTypeLookupTable.length) {
+      return -1;
+    }
+    if(channelIndex < 0 || channelIndex >= openCVTypeLookupTable[0].length) {
+      return -1;
     }
     
-    
-    //Step 2: build FloatImage from converted mat and input parameters which help to define the header.
-    int height = convertedMat.rows();
-    int width = convertedMat.cols();   
-    int channels = convertedMat.channels();
-    FloatImage unconvertedImage = new FloatImage(width, height, channels);
-    unconvertedImage.setHeader(new HipiImageHeader(outputHipiImageFormat, outputHipiColorSpace, width, height, channels, null, null));
-
-    float [] data = new float[(int) (convertedMat.total() * convertedMat.channels())];
-    ((FloatBuffer) convertedMat.createBuffer()).get(data);
-    unconvertedImage.setData(data);
-    return unconvertedImage;
+    return openCVTypeLookupTable[depthIndex][channelIndex];
   }
   
-  public static Mat convertFloatImageToMat(FloatImage inputImage, OpenCVOutputColorSpace openCVOutputColorSpace) throws IOException {
+  public static Mat convertRasterImageToMat(RasterImage image) throws IllegalArgumentException {
     
-    //Step 1: convert FloatImage to a Mat of comparable type
-    Mat unconvertedMat;
+    if(image == null) {
+      throw new IllegalArgumentException("Input RasterImage is null");
+    }
+    if(image.getWidth() <= 0 || image.getHeight() <= 0) {
+      throw new IllegalArgumentException("Input RasterImage has invalid dimensions [" + image.getWidth() + "," + image.getHeight() + "]");
+    }
     
-    int rows = inputImage.getHeight();
-    int cols = inputImage.getWidth();
-    HipiColorSpace colorSpace = inputImage.getColorSpace();
-    switch(colorSpace) {
-      case RGB:
-        unconvertedMat = new Mat(rows, cols, opencv_core.CV_32FC3);
+    int numBands = image.getNumBands();
+    int pixelArrayDataType = image.getPixelArray().getDataType();
+    int openCVType = generateOpenCVType(pixelArrayDataType, numBands);
+    if(openCVType == -1) {
+      throw new IllegalArgumentException("Invalid PixelArray data type [" + pixelArrayDataType + "] and / or RasterImage numBands [" + numBands + "]");
+    }
+    
+    Mat mat = new Mat(image.getHeight(), image.getWidth(), openCVType);
+    
+    byte[] data = image.getPixelArray().getByteArray();
+    
+    switch(opencv_core.CV_MAT_DEPTH(mat.type())) {
+      case opencv_core.CV_8U:
+      case opencv_core.CV_8S:
+        ((ByteBuffer)mat.createBuffer()).put(data);
         break;
-      case LUM:
-        unconvertedMat = new Mat(rows, cols, opencv_core.CV_32FC1);
+      case opencv_core.CV_16U:
+      case opencv_core.CV_16S:      
+        ((ShortBuffer)mat.createBuffer()).put(ByteUtils.byteArrayToShortArray(data));
+        break;
+      case opencv_core.CV_32S:
+        ((IntBuffer)mat.createBuffer()).put(ByteUtils.byteArrayToIntArray(data));
+        break;
+      case opencv_core.CV_32F:
+        ((FloatBuffer)mat.createBuffer()).put(ByteUtils.byteArrayToFloatArray(data));
+        break;
+      case opencv_core.CV_64F:
+        ((DoubleBuffer)mat.createBuffer()).put(ByteUtils.byteArrayToDoubleArray(data));
         break;
       default:
-        throw new IOException("Unsupported HipiColorSpace [" + colorSpace + "].");
+        throw new IllegalArgumentException("Unsupported matrix depth [" + opencv_core.CV_MAT_DEPTH(mat.type()) + "].");
     }
     
-    ((FloatBuffer) unconvertedMat.createBuffer()).put(inputImage.getData());
-    
-    
-    //Step 2: perform mat conversion (if necessary)
-    
-    switch(openCVOutputColorSpace) {
-      case OPENCV_RGB:
-        if(colorSpace != HipiColorSpace.RGB) { //convert to rgb only if mat isn't already rgb
-          Mat rgbMat = new Mat(unconvertedMat.rows(), unconvertedMat.cols(), opencv_core.CV_32FC3, new Scalar(0.0));
-          cvtColor(unconvertedMat, rgbMat, CV_GRAY2RGB);
-          return rgbMat;
-        } else {
-          return unconvertedMat;
-        }
-      case OPENCV_GRAY:
-        if(colorSpace != HipiColorSpace.LUM) { //convert to grayscale only if mat isn't already lum
-          Mat grayMat = new Mat(unconvertedMat.rows(), unconvertedMat.cols(), opencv_core.CV_32FC1, new Scalar(0.0));
-          cvtColor(unconvertedMat, grayMat, CV_RGB2GRAY);
-          return grayMat;
-        } else {
-          return unconvertedMat;
-        }
-      default:
-        throw new IOException("Unsupported opencv color space specified for output mat [" + openCVOutputColorSpace + "].");
-    }
+    return mat;
   }
+  
+//  public static Mat convertFloatImageToMat(FloatImage inputImage, OpenCVOutputColorSpace openCVOutputColorSpace) throws IOException {
+//    
+//    //Step 1: convert FloatImage to a Mat of comparable type
+//    Mat unconvertedMat;
+//    
+//    int rows = inputImage.getHeight();
+//    int cols = inputImage.getWidth();
+//    HipiColorSpace colorSpace = inputImage.getColorSpace();
+//    switch(colorSpace) {
+//      case RGB:
+//        unconvertedMat = new Mat(rows, cols, opencv_core.CV_32FC3);
+//        break;
+//      case LUM:
+//        unconvertedMat = new Mat(rows, cols, opencv_core.CV_32FC1);
+//        break;
+//      default:
+//        throw new IOException("Unsupported HipiColorSpace [" + colorSpace + "].");
+//    }
+//    
+//    ((FloatBuffer) unconvertedMat.createBuffer()).put(inputImage.getData());
+//    
+//    
+//    //Step 2: perform mat conversion (if necessary)
+//    
+//    switch(openCVOutputColorSpace) {
+//      case OPENCV_RGB:
+//        if(colorSpace != HipiColorSpace.RGB) { //convert to rgb only if mat isn't already rgb
+//          Mat rgbMat = new Mat(unconvertedMat.rows(), unconvertedMat.cols(), opencv_core.CV_32FC3, new Scalar(0.0));
+//          cvtColor(unconvertedMat, rgbMat, CV_GRAY2RGB);
+//          return rgbMat;
+//        } else {
+//          return unconvertedMat;
+//        }
+//      case OPENCV_GRAY:
+//        if(colorSpace != HipiColorSpace.LUM) { //convert to grayscale only if mat isn't already lum
+//          Mat grayMat = new Mat(unconvertedMat.rows(), unconvertedMat.cols(), opencv_core.CV_32FC1, new Scalar(0.0));
+//          cvtColor(unconvertedMat, grayMat, CV_RGB2GRAY);
+//          return grayMat;
+//        } else {
+//          return unconvertedMat;
+//        }
+//      default:
+//        throw new IOException("Unsupported opencv color space specified for output mat [" + openCVOutputColorSpace + "].");
+//    }
+//  }
     
 }
--- core/src/test/java/org/hipi/test/OpenCVUtilsTestCase.java
+++ core/src/test/java/org/hipi/test/OpenCVUtilsTestCase.java
@@ -21,8 +21,10 @@ import org.bytedeco.javacpp.indexer.FloatIndexer;
 import org.hipi.image.ByteImage;
 import org.hipi.image.FloatImage;
 import org.hipi.image.HipiImageFactory;
+import org.hipi.image.HipiImageHeader;
 import org.hipi.image.HipiImageHeader.HipiColorSpace;
 import org.hipi.image.HipiImageHeader.HipiImageFormat;
+import org.hipi.image.PixelArray;
 import org.hipi.image.RasterImage;
 import org.hipi.image.io.ImageDecoder;
 import org.hipi.image.io.PpmCodec;
@@ -34,6 +36,7 @@ import org.junit.Test;
 public class OpenCVUtilsTestCase {
   
   private static final float delta = 0.1f;
+  
   
   private ArrayList<Mat> createTestMatImages() throws IllegalArgumentException, IOException {
     ArrayList<Mat> mats = new ArrayList<Mat>();
@@ -80,96 +83,171 @@ public class OpenCVUtilsTestCase {
     return floatImages;
   }
   
-  @Test
-  public void testCreateFloatImageFromValidMat() throws IOException {
-    int imageCount = 1;
-    for(Mat mat : createTestMatImages()) {
- 
-      System.out.println("Testing mat number " + imageCount);
-      FloatImage image = OpenCVUtils.convertMatToFloatImage(mat, HipiColorSpace.RGB, HipiImageFormat.UNDEFINED);
-      
-      System.out.println("Testing image dimensions");
-      assertEquals("Widths of float image and mat are not equivalent.", mat.cols(), image.getWidth());
-      assertEquals("Heights of float image and mat are not equivalent.", mat.rows(), image.getHeight());
-      
-      System.out.println("Testing image channels");
-      assertEquals("Incorrect number of channels in new FloatImage.", mat.channels(), image.getNumBands());
-      switch(mat.channels()) {
-        case 1:
-          assertEquals("Incorrect color space in new FloatImage: " + image.getColorSpace(), HipiColorSpace.LUM, image.getColorSpace());
-          break;
-        case 3:
-          assertEquals("Incorrect color space in new FloatImage: " + image.getColorSpace(), HipiColorSpace.RGB, image.getColorSpace());
-          break;
-        default:
-          fail("Unsupported number of channels: " + mat.channels());
-          break;
-      }
-      
-      System.out.println("Testing image contents");
-      float[] imageData = image.getData();
-      
-      float[] baselineDataAsFloatArray = new float[imageData.length];
-      ((FloatBuffer)mat.createBuffer()).get(baselineDataAsFloatArray);
-      
-      assertEquals("Data arrays have different lengths.", baselineDataAsFloatArray.length, imageData.length);
-      assertArrayEquals("Data arrays are not equivalent.", baselineDataAsFloatArray, imageData, delta);
-      
-      imageCount++;    
-      System.out.println("");
+  private FloatImage createTestFloatImage() throws IllegalArgumentException, IOException {
+    
+    
+    ImageDecoder ppmDecoder = PpmCodec.getInstance();
+    assertNotNull(ppmDecoder);
+    
+    
+    File file = new File("../testdata/jpeg-rgb").listFiles()[0];
+    if (file.isFile() && file.getName().endsWith("_photoshop.ppm")) {
+      String ppmPath = file.getPath();
+      System.out.println(ppmPath);
+      FileInputStream ppmInputStream = new FileInputStream(ppmPath);
+      assertNotNull(ppmInputStream);
+      HipiImageFactory imageFactory = HipiImageFactory.getFloatImageFactory();
+      assertNotNull(imageFactory);
+      FloatImage image = (FloatImage)ppmDecoder.decodeHeaderAndImage(ppmInputStream, imageFactory, false);
+      return image;
     }
+    return null;
   }
   
+  @Test
+  public void testGenerateOpenCVTypeInvalidPixelArrayType() {
+    assertEquals("Invalid lookup was not caught.", -1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_UNDEFINED, 1));
+  }
   
   @Test
-  public void testCreateMatFromValidFloatImage() throws IOException {
-    int imageCount = 1;
-    for(FloatImage image : createTestFloatImages()) {
-      System.out.println("Testing floatImage number " + imageCount);
-      Mat mat;
-      
-      switch(image.getColorSpace()) {
-        case RGB:
-          mat = OpenCVUtils.convertFloatImageToMat(image, OpenCVUtils.OpenCVOutputColorSpace.OPENCV_RGB);
-          break;
-        case LUM:
-          mat = OpenCVUtils.convertFloatImageToMat(image, OpenCVUtils.OpenCVOutputColorSpace.OPENCV_GRAY);
-          break;
-        default:
-          throw new IOException("Unsupported HipiColorSpace [" + image.getColorSpace() + "].");
-      }
-      
-      System.out.println("Testing image dimensions");
-      assertEquals("Widths of float image and mat are not equivalent.", image.getWidth(), mat.cols());
-      assertEquals("Heights of float image and mat are not equivalent.", image.getHeight(), mat.rows());
-      
-      System.out.println("Testing image channels / data type");
-      switch(image.getColorSpace()) {
-        case RGB:
-          assertEquals("Incorrect data type in new mat.", opencv_core.CV_32FC3, mat.type());
-          assertEquals("Incorrect number of channels in new mat.", 3, mat.channels());
-          break;
-        case LUM:
-          assertEquals("Incorrect data type in new mat.", opencv_core.CV_32FC1, mat.type());
-          assertEquals("Incorrect number of channels in new mat.", 1, mat.channels());
-          break;
-        case UNDEFINED:
-        default:
-          fail("Unsupported color space: " + image.getColorSpace());
-          break;
-      }
-      
-      System.out.println("Testing image contents");
-      float[] baselineData = image.getData();
-     
-      float[] matDataAsFloatArray = new float[baselineData.length];
-      ((FloatBuffer)mat.createBuffer()).get(matDataAsFloatArray);
-      
-      assertEquals("Data arrays have different lengths.", baselineData.length, matDataAsFloatArray.length);
-      assertArrayEquals("Data arrays are not equivalent.", baselineData, matDataAsFloatArray, delta);
-      
-      imageCount++;    
-      System.out.println("");
-    }
+  public void testGenerateOpenCVTypeInvalidChannels() {
+    assertEquals("Invalid lookup was not caught.", -1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_BYTE, -1));
   }
+  
+  @Test public void testGenerateOpenCVTypeIntegrityOfLookupTable() {
+    assertEquals("Lookup of opencv_core.CV_8UC1 failed.", opencv_core.CV_8UC1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_BYTE, 1));
+    assertEquals("Lookup of opencv_core.CV_8UC2 failed.", opencv_core.CV_8UC2, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_BYTE, 2));
+    assertEquals("Lookup of opencv_core.CV_8UC3 failed.", opencv_core.CV_8UC3, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_BYTE, 3));
+    assertEquals("Lookup of opencv_core.CV_8UC4 failed.", opencv_core.CV_8UC4, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_BYTE, 4));
+    
+    assertEquals("Lookup of opencv_core.CV_16UC1 failed.", opencv_core.CV_16UC1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_USHORT, 1));
+    assertEquals("Lookup of opencv_core.CV_16UC2 failed.", opencv_core.CV_16UC2, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_USHORT, 2));
+    assertEquals("Lookup of opencv_core.CV_16UC3 failed.", opencv_core.CV_16UC3, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_USHORT, 3));
+    assertEquals("Lookup of opencv_core.CV_16UC4 failed.", opencv_core.CV_16UC4, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_USHORT, 4));
+    
+    assertEquals("Lookup of opencv_core.CV_16SC1 failed.", opencv_core.CV_16SC1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_SHORT, 1));
+    assertEquals("Lookup of opencv_core.CV_16SC2 failed.", opencv_core.CV_16SC2, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_SHORT, 2));
+    assertEquals("Lookup of opencv_core.CV_16SC3 failed.", opencv_core.CV_16SC3, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_SHORT, 3));
+    assertEquals("Lookup of opencv_core.CV_16SC4 failed.", opencv_core.CV_16SC4, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_SHORT, 4));
+    
+    assertEquals("Lookup of opencv_core.CV_32SC1 failed.", opencv_core.CV_32SC1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_INT, 1));
+    assertEquals("Lookup of opencv_core.CV_32SC2 failed.", opencv_core.CV_32SC2, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_INT, 2));
+    assertEquals("Lookup of opencv_core.CV_32SC3 failed.", opencv_core.CV_32SC3, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_INT, 3));
+    assertEquals("Lookup of opencv_core.CV_32SC4 failed.", opencv_core.CV_32SC4, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_INT, 4));
+    
+    assertEquals("Lookup of opencv_core.CV_32FC1 failed.", opencv_core.CV_32FC1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_FLOAT, 1));
+    assertEquals("Lookup of opencv_core.CV_32FC2 failed.", opencv_core.CV_32FC2, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_FLOAT, 2));
+    assertEquals("Lookup of opencv_core.CV_32FC3 failed.", opencv_core.CV_32FC3, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_FLOAT, 3));
+    assertEquals("Lookup of opencv_core.CV_32FC4 failed.", opencv_core.CV_32FC4, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_FLOAT, 4));
+    
+    assertEquals("Lookup of opencv_core.CV_64FC1 failed.", opencv_core.CV_64FC1, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_DOUBLE, 1));
+    assertEquals("Lookup of opencv_core.CV_64FC2 failed.", opencv_core.CV_64FC2, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_DOUBLE, 2));
+    assertEquals("Lookup of opencv_core.CV_64FC3 failed.", opencv_core.CV_64FC3, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_DOUBLE, 3));
+    assertEquals("Lookup of opencv_core.CV_64FC4 failed.", opencv_core.CV_64FC4, OpenCVUtils.generateOpenCVType(PixelArray.TYPE_DOUBLE, 4));
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testConvertRasterImageToMatNullInput() {
+    OpenCVUtils.convertRasterImageToMat(null);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testConvertRasterImageToMatInvalidSize() throws IllegalArgumentException, IOException {
+    FloatImage image = createTestFloatImage();
+    image.setHeader(new HipiImageHeader(HipiImageFormat.JPEG, HipiColorSpace.RGB, 0, 0, 0, null, null));
+    OpenCVUtils.convertRasterImageToMat(new FloatImage());
+  }
+  
+//  @Test
+//  public void testCreateFloatImageFromValidMat() throws IOException {
+//    int imageCount = 1;
+//    for(Mat mat : createTestMatImages()) {
+// 
+//      System.out.println("Testing mat number " + imageCount);
+//      FloatImage image = OpenCVUtils.convertMatToFloatImage(mat, HipiColorSpace.RGB, HipiImageFormat.UNDEFINED);
+//      
+//      System.out.println("Testing image dimensions");
+//      assertEquals("Widths of float image and mat are not equivalent.", mat.cols(), image.getWidth());
+//      assertEquals("Heights of float image and mat are not equivalent.", mat.rows(), image.getHeight());
+//      
+//      System.out.println("Testing image channels");
+//      assertEquals("Incorrect number of channels in new FloatImage.", mat.channels(), image.getNumBands());
+//      switch(mat.channels()) {
+//        case 1:
+//          assertEquals("Incorrect color space in new FloatImage: " + image.getColorSpace(), HipiColorSpace.LUM, image.getColorSpace());
+//          break;
+//        case 3:
+//          assertEquals("Incorrect color space in new FloatImage: " + image.getColorSpace(), HipiColorSpace.RGB, image.getColorSpace());
+//          break;
+//        default:
+//          fail("Unsupported number of channels: " + mat.channels());
+//          break;
+//      }
+//      
+//      System.out.println("Testing image contents");
+//      float[] imageData = image.getData();
+//      
+//      float[] baselineDataAsFloatArray = new float[imageData.length];
+//      ((FloatBuffer)mat.createBuffer()).get(baselineDataAsFloatArray);
+//      
+//      assertEquals("Data arrays have different lengths.", baselineDataAsFloatArray.length, imageData.length);
+//      assertArrayEquals("Data arrays are not equivalent.", baselineDataAsFloatArray, imageData, delta);
+//      
+//      imageCount++;    
+//      System.out.println("");
+//    }
+//  }
+//  
+  
+//  @Test
+//  public void testCreateMatFromValidFloatImage() throws IOException {
+//    int imageCount = 1;
+//    for(FloatImage image : createTestFloatImages()) {
+//      System.out.println("Testing floatImage number " + imageCount);
+//      Mat mat;
+//      
+//      switch(image.getColorSpace()) {
+//        case RGB:
+//          mat = OpenCVUtils.convertFloatImageToMat(image, OpenCVUtils.OpenCVOutputColorSpace.OPENCV_RGB);
+//          break;
+//        case LUM:
+//          mat = OpenCVUtils.convertFloatImageToMat(image, OpenCVUtils.OpenCVOutputColorSpace.OPENCV_GRAY);
+//          break;
+//        default:
+//          throw new IOException("Unsupported HipiColorSpace [" + image.getColorSpace() + "].");
+//      }
+//      
+//      System.out.println("Testing image dimensions");
+//      assertEquals("Widths of float image and mat are not equivalent.", image.getWidth(), mat.cols());
+//      assertEquals("Heights of float image and mat are not equivalent.", image.getHeight(), mat.rows());
+//      
+//      System.out.println("Testing image channels / data type");
+//      switch(image.getColorSpace()) {
+//        case RGB:
+//          assertEquals("Incorrect data type in new mat.", opencv_core.CV_32FC3, mat.type());
+//          assertEquals("Incorrect number of channels in new mat.", 3, mat.channels());
+//          break;
+//        case LUM:
+//          assertEquals("Incorrect data type in new mat.", opencv_core.CV_32FC1, mat.type());
+//          assertEquals("Incorrect number of channels in new mat.", 1, mat.channels());
+//          break;
+//        case UNDEFINED:
+//        default:
+//          fail("Unsupported color space: " + image.getColorSpace());
+//          break;
+//      }
+//      
+//      System.out.println("Testing image contents");
+//      float[] baselineData = image.getData();
+//     
+//      float[] matDataAsFloatArray = new float[baselineData.length];
+//      ((FloatBuffer)mat.createBuffer()).get(matDataAsFloatArray);
+//      
+//      assertEquals("Data arrays have different lengths.", baselineData.length, matDataAsFloatArray.length);
+//      assertArrayEquals("Data arrays are not equivalent.", baselineData, matDataAsFloatArray, delta);
+//      
+//      imageCount++;    
+//      System.out.println("");
+//    }
+//  }
 }
